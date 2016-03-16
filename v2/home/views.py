e5785734bf325744cac01e4cfbc9743f46070aa0
Alex Koval <alex@ua2web.com>
2016-03-16 08:23:18 +0200

    examples of rendering body or topless, e.g: http://127.0.0.1:8000/?layout=body http://127.0.0.1:8000/?layout=topless

--- v2/home/views.py
+++ v2/home/views.py
@@ -9,3 +9,17 @@ class HomeView(TemplateView):
         if self.request.flavour == 'mobile':
             prefix = 'mobile'
         return ['%s/%s' % (prefix, self.template_name)]
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['sidebar'] = True
+        if 'layout' in self.request.GET:
+            if self.request.GET['layout'] == 'topless':
+                context_data['topless'] = True
+            elif self.request.GET['layout'] == 'body':
+                context_data['topless'] = True
+                context_data['bottomless'] = True
+                context_data['sidebar'] = False
+
+        print(context_data)
+        return context_data
